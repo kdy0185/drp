ec4c64f5e4c58a9eaf1fdc82c4c2838adcbee125
D.W Kang <kdy0185@gmail.com>
2022-03-07 19:13:43 +0900

    Docs #7 - 주석 추가
    * 그룹 관리 Repository 부분 주석 추가

--- src/main/java/com/jsplan/drp/domain/sys/usermng/repository/UserGrpMngCustomRepository.java
+++ src/main/java/com/jsplan/drp/domain/sys/usermng/repository/UserGrpMngCustomRepository.java
@@ -1,13 +1,23 @@
 package com.jsplan.drp.domain.sys.usermng.repository;
 
 import com.jsplan.drp.domain.sys.usermng.entity.UserGrpMng;
-import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+/**
+ * @Class : UserGrpMngCustomRepository
+ * @Author : KDW
+ * @Date : 2022-03-07
+ * @Description : 그룹 관리 Custom Repository
+ */
 public interface UserGrpMngCustomRepository {
 
-    List<UserGrpMng> searchExcel();
-
+    /**
+     * <p>그룹 목록</p>
+     *
+     * @param grpNm (그룹명)
+     * @param pageable (페이징 정보)
+     * @return Page (페이징 목록)
+     */
     Page<UserGrpMng> searchPageList(String grpNm, Pageable pageable);
 }
